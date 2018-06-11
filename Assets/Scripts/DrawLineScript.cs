02404868c7ef811b099f7873279b32dbaca44465
Bin-Jing <jeff20001234@yahoo.com.tw>
2018-06-11 14:25:52 +0800

    fix line material

--- Assets/Scripts/DrawLineScript.cs
+++ Assets/Scripts/DrawLineScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DrawLineScript : MonoBehaviour {
-    
+    public Material lineMaterial;
     public Transform t2;
     GameObject player;
     Transform tar;
@@ -19,7 +19,7 @@ public class DrawLineScript : MonoBehaviour {
 	void Update () {
         if(tar == null)
             tar = player.transform;
-        DrawLine(tar.position, this.transform.position, Color.green, Time.deltaTime/2);
+        DrawLine(tar.position, this.transform.position, new Color(0.6F, 0.6F, 0.4F, 0.5F), Time.deltaTime/2);
 
 	}
 	private void OnTriggerEnter(Collider other)
@@ -35,9 +35,9 @@ public class DrawLineScript : MonoBehaviour {
         myLine.transform.position = start;
         myLine.AddComponent<LineRenderer>();
         LineRenderer lr = myLine.GetComponent<LineRenderer>();
-        lr.material = new Material(Shader.Find("Particles/Alpha Blended Premultiply"));
+        lr.material = lineMaterial;
         lr.SetColors(color, color);
-        lr.SetWidth(0.2f, 0.2f);
+        lr.SetWidth(0.05f, 0.05f);
         lr.SetPosition(0, start);
         lr.SetPosition(1, end);
         GameObject.Destroy(myLine, duration);
